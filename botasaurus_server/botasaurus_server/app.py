d7cf09aed2472645a22318e2dd6fe524ad5ab305
root <root@vmi2121224.contaboserver.net>
2024-09-18 16:04:42 +0200

    Modified run.py to bind to 0.0.0.0

--- botasaurus_server/botasaurus_server/app.py
+++ botasaurus_server/botasaurus_server/app.py
@@ -1,8 +1,9 @@
 from bottle import run, BaseRequest
 from .env import is_in_kubernetes, is_worker, is_master
 
-BaseRequest.MEMFILE_MAX = 100 * 1024 * 1024 # 100 MB Max Data Payload
+BaseRequest.MEMFILE_MAX = 100 * 1024 * 1024  # 100 MB Max Data Payload
 
+# Import routes based on the context (master, worker, or task)
 if is_master:
     from . import master_routes
 elif is_worker:
@@ -13,13 +14,21 @@ else:
 from .executor import executor
 
 def run_backend():
+    # Load and start the executor
     executor.load()
     executor.start()
 
-    host = '0.0.0.0' if is_in_kubernetes else '127.0.0.1'
+    # Set host to '0.0.0.0' when in Kubernetes or Docker, and '127.0.0.1' otherwise
+    host = '0.0.0.0'  # Ensure it binds to all interfaces for Docker accessibility
     debug = False
+
+    # Start the Bottle server
     run(
         host=host,
         port=8000,
         debug=debug
     )
+
+# Ensure the backend runs if this script is executed directly
+if __name__ == "__main__":
+    run_backend()
